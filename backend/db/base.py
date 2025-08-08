abff1d1a74cff0d84a1d622b782e157b5b707aa7
kandibobe <kirgyzmahmet@gmail.com>
2025-08-08 13:30:14 +0200

    refactor: consolidate models and schemas

--- backend/db/base.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.orm import declarative_base
-
-# Base - это просто фабрика, от которой будут наследоваться все наши модели.
-# Он не должен импортировать модели сам.
-Base = declarative_base()
